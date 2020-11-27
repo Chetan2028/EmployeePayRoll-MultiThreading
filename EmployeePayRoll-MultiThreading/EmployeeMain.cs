31ae448a3a2ab3bcb92c7dad35b8175ee016256a
Chetan2028 <bmchetan2028@gmail.com>
2020-11-28 00:39:42 +0530

    Add project files.

---
+++ EmployeePayRoll-MultiThreading/EmployeeMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayRoll_MultiThreading
+{
+    class EmployeeMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
